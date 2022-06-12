5742ecd9acd0e72e2feca7e7263b0336d8b8dd9a
chx9 <chenghuan1999@126.com>
2022-06-12 20:12:22 +0800

    -

--- 02intermediate/cnn/main.py
+++ 02intermediate/cnn/main.py
@@ -1,6 +1,78 @@
+import enum
+import struct
+from turtle import forward
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
-foo = torch.rand(1,3,224, 224).to('mps')
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+lr = 0.001
+device = 'mps'
+# device = 'cpu'
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True, 
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10) -> None:
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16,kernel_size=5, stride=1, padding=2), 
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)     
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc = nn.Linear(7*7*32, num_classes)
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+model = ConvNet(num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+total_step = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (imgs, labels) in enumerate(train_loader):
+        imgs = imgs.to(device)
+        lables = labels.to(device)
+        outputs = model(imgs)
+        
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if(i+1) % 100 == 0:
+             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
